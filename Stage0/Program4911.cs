8fc56a432fa131db96dd65cb4f10c90f48ca561f
ayala369 <144127118+ayala369@users.noreply.github.com>
2024-12-26 23:11:27 +0200

    stage 0

---
+++ Stage0/Program4911.cs
@@ -0,0 +1,19 @@
+﻿namespace Stage0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome4911();
+            Welcome1005();
+            Console.ReadKey();
+        }
+        static partial void Welcome1005();
+        private static void Welcome4911()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0} , welcome to my first console application", name);
+        }
+    }
+}
